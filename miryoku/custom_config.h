515bebba241fbb4ba41122ae041ec09181da0ed4
Guillaume <guillaumeleroy33@gmail.com>
2025-06-26 18:10:21 +0900

    fix keyname

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,6 +2,7 @@
 // https://github.com/manna-harbour/miryoku
 
 #define XXX &none
+#define HYPER &kp LG(LC(LA(LSHIFT)))
 
 #define MIRYOKU_LAYOUTMAPPING_CORNE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -9,10 +10,10 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp ESC                    K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-&kp LG(LC(LA(LSHIFT)))     K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp LG(LC(LA(LSHIFT))) \
-&kp LG                     K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp LG \
-                                     K32  K33  K34       K35  K36  K37
+&kp ESC   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
+HYPER     K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  HYPER \
+&kp LGUI  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp LGUI \
+                    K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
